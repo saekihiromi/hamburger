3e1987ee31e90386db17c5f81de5c0f82d84ad8a
saekihiromi <80839660+saekihiromi@users.noreply.github.com>
2022-02-23 16:00:51 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data(){
+        return{
+            open: false
+        }
+    },
+    methods:{
+        toggleButton(){
+            this.open = !this.open
+        }
+    }
+
+
+}).mount('#app')
